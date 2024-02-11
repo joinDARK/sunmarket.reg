27d6e3656e5a0f731baad4eef33f8d8be7a14a0c
Eduard <frkd10wot@ya.ru>
2024-02-11 22:26:38 +0300

    Firts commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+      <div>
+        <h1>Yes sirr</h1>
+        <p>YEs ssiiirr</p>
+        <p>ddd</p>
+        <p>ddddd</p>
+      </div>
+      
+  )
+}
+
+export default App
